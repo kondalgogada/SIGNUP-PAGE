46a51085e0bf03d3f5ac7d5df79fd5018ad71cf2
kondalgogada <gkrao1215@gmail.com>
2019-05-01 05:48:52 -0700

    signup page purposes

---
+++ registation/src/app/home/home.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SWITCH_COMPILE_NGMODULE__POST_R3__ } from '@angular/core/src/metadata/ng_module';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+}
+
